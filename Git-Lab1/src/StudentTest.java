2836d5542a1ee6b98f22ad7881427f32a4dc9546
YakisobaUFO <rtreacher@yahoo.com>
2023-09-17 21:57:35 +0200

    Added testing in StudentTest.java.  Modified testGetClasses() and testGetAge()

--- Git-Lab1/src/StudentTest.java
+++ Git-Lab1/src/StudentTest.java
@@ -22,10 +22,20 @@ public class StudentTest {
 	@Test
 	void testGetClasses() {
 		 //TODO: test that the classes array returned is correct
+		String[] strExpectedBob = {"COSC 222","COSC 311", "MATH 200", "MATH 220"};
+		String[] strExpectedBill = {"COSC 222", "COSC 404", "ENGL 112"};
+		String[] strExpectedBen = {"COSC 222", "COSC 111", "MATH 200", "PHYS 101"};
+		assertArrayEquals(bob.getClasses(), strExpectedBob );
+		assertArrayEquals(bill.getClasses(), strExpectedBill );
+		assertArrayEquals(ben.getClasses(), strExpectedBen );
 	}
 
 	@Test
 	void testGetAge() {
 		//TODO: test that the age returned is correct
+		assertEquals(bob.getAge(),18);
+		assertEquals(bill.getAge(),19);
+		assertEquals(ben.getAge(),24);
+		
 	}
 }
